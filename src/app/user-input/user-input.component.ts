3a339f40594e201c2c06b58510f14e47483b35e5
pankaj485 <pankajsamar2@gmail.com>
2024-09-05 19:11:21 +0530

    add data binding on user input fomrs

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user-input',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user-input.component.html',
-  styleUrl: './user-input.component.css'
+  styleUrl: './user-input.component.css',
 })
 export class UserInputComponent {
+  initialInvestment: number = 0;
+  annualInvestment: number = 0;
+  expectedReturn: number = 0;
+  duration: number = 0;
 
+  onFormSubmit() {
+    console.log('form is submitted');
+    console.log('initialInvestment: ', this.initialInvestment);
+    console.log('annualInvestment: ', this.annualInvestment);
+    console.log('expectedReturn: ', this.expectedReturn);
+    console.log('duration: ', this.duration);
+  }
 }
